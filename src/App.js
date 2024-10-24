3c0b494343485ce8632ce9cda7922ce24808df03
BHUVI1529 <srinivasanbhuvana7@gmail.com>
2024-10-24 11:58:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './components/Login';
+import Register from './components/Register';
+import AdminDashboard from './components/AdminDashboard';
+import UserDashboard from './components/UserDashboard';
+import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar />
+          <Routes>
+          <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        <Route 
+          path="/admin" element={<PrivateRoute role="ADMIN"><AdminDashboard /></PrivateRoute> } />
+        <Route 
+          path="/user" element={<PrivateRoute role="USER"><UserDashboard /></PrivateRoute> }/>
+        
+          </Routes>
+    
+    </Router>
+    
   );
 }
 
