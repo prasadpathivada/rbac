7f31fcf67058dc66697c572f25fbdc1f0fc98e54
prasad <prasadpathivada01@gmail.com>
2024-10-28 11:30:20 +0530

    Add backend code

---
+++ src/main/java/com/example/OJT/OjtApplication.java
@@ -0,0 +1,14 @@
+package com.example.OJT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjtApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(OjtApplication.class, args);
+	}
+
+}
